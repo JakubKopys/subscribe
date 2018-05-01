bae9729a8168d554bf04c3552c26be81f111e192
Jakub <jakub.kopys@tlen.pl>
2018-05-01 23:49:59 +0200

    Specs for creating supscriptions

--- spec/requests/api/v1/subscriptions_controller_spec.rb
+++ spec/requests/api/v1/subscriptions_controller_spec.rb
@@ -1,10 +1,57 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
+require 'subscriptions/payment_api'
+
 RSpec.describe Api::V1::SubscriptionsController, type: :request do
-  describe 'GET #index' do
+  xdescribe 'GET #index' do
   end
 
   describe 'POST #create' do
+    context 'with wrong payment api credentials' do
+      it 'is unauthorized and returns errors' do
+        expect(Subscriptions::PaymentApi).to receive(:make_payment).and_return true
+      end
+    end
 
+    context 'when payment succeeds' do
+      it 'is created and saves Subscription' do
+        expect(Subscriptions::PaymentApi).to receive(:make_payment).and_return true
+
+        create_params = {
+          credit_card_number: 'foobar',
+          cardholder_name: 'foobar',
+          card_cvv: '123',
+          expiration_year: '30',
+          expiration_month: '11'
+        }
+
+        expect do
+          post '/api/v1/subscriptions', params: create_params
+        end.to change(Subscription, :count).by 1
+
+        expect(response).to be_created
+      end
+    end
+
+    context 'when payment fails' do
+      it 'is unprocessable and does not save Subscription' do
+        expect(Subscriptions::PaymentApi).to receive(:make_payment).and_return false
+
+        create_params = {
+          credit_card_number: 'foobar',
+          cardholder_name: 'foobar',
+          card_cvv: '123',
+          expiration_year: '30',
+          expiration_month: '11'
+        }
+
+        expect do
+          post '/api/v1/subscriptions', params: create_params
+        end.not_to change(Subscription, :count)
+
+        expect(response).to be_unprocessable
+      end
+    end
   end
 end
